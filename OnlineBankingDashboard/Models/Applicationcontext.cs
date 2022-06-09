b73fc1cc20bb782200d7d7eec43c5e460754f25d
mahesh9797 <mahesh9797@freetrial>
2022-06-09 21:05:52 +0530

    Add project files.

---
+++ OnlineBankingDashboard/Models/Applicationcontext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace OnlineBankingDashboard.Models
+{
+    public class Applicationcontext:DbContext
+    {
+        public Applicationcontext(DbContextOptions options) : base(options)
+        {
+
+        }
+        public DbSet<Account> accounts { get; set; }
+
+        public DbSet<User> users { get; set; }
+    }
+}
